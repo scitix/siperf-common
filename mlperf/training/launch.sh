e18e4fdaba787c78246b05cf692fc3d136395e12
Zhongqi An <zqan@scitix.ai>
2024-12-19 15:49:22 +0800

    add launch script for mpirun booting torch training benchmarks.

---
+++ mlperf/training/launch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024, Scitix Tech PTE. LTD. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+TEST_DIR=$(cd $(dirname $0) && pwd)
+export CUDA_DEVICE_MAX_CONNECTION=1
+export ENABLE_RERUNS=1
+export CREATE_TENSORBOARD_LOGGER=True
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export GRADIENT_ACCUMULATION_STEPS=2
+source $TEST_DIR/eos_n32_ngc24.09_nemo/config_DGXH100_32x8x256x4x8_mbs2.sh
+bash $TEST_DIR/mpirun2pytorch $TEST_DIR/run_and_time_scitix.sh
